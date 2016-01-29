bb534104cb54e1b129589333d73d002347856120
Thomas Weber <ginko@cg.tuwien.ac.at>
2016-01-29 14:48:40 -0800

    Provide first simple game logic prototype

--- structure/scripts/main.js
+++ structure/scripts/main.js
@@ -1,24 +1,29 @@
 
 function clear_error() {
-    document.getElementById("error").innerHTML = "";
+    //document.getElementById("error").innerHTML = "";
 }
 
 function error(msg) {
-    document.getElementById("error").innerHTML = "<font color=\"red\">" + msg + "</font>";
+    //document.getElementById("error").innerHTML = "<font color=\"red\">" + msg + "</font>";
+    log("<font color=\"red\">" + msg + "</font>");
 }
 
 function log(msg) {
-    document.getElementById("log").innerHTML += "<br/>" + msg;
+    document.getElementById("log").innerHTML = msg + "<br/>" + document.getElementById("log").innerHTML;
 }
 
 function info(msg) {
     document.getElementById("info").innerHTML = msg;
 }
 
+function randint(i) {
+    return Math.floor((i + 1) * Math.random());
+}
+
 function getRandomSubarray(arr, size) {
     var shuffled = arr.slice(0), i = arr.length, temp, index;
     while (i--) {
-        index = Math.floor((i + 1) * Math.random());
+        index = randint(i);
         temp = shuffled[index];
         shuffled[index] = shuffled[i];
         shuffled[i] = temp;
@@ -27,7 +32,7 @@ function getRandomSubarray(arr, size) {
 }
 
 function contains(arr, elem) {
-    return (arr.lastIndexOf(elem) > 0);
+    return (arr.lastIndexOf(elem) >= 0);
 }
 
 function Choice (previous_choices) {
@@ -41,8 +46,20 @@ function Choice (previous_choices) {
         for (i = 0; i < this.options.length; ++i) {            
             option_str += this.options[i] + " "
         }
-        info("Current options: " + option_str);
+        if (this.chosen != null) {
+            info("Current option: " + option_str);
+        } else {
+            info("<font color=\"green\">New option: " + option_str + "</font>");
+        }
     };
+
+    this.choose = function (pick) {
+        if (this.chosen == null) {
+            this.chosen = pick;
+        } else if (this.chosen != pick) {
+            error("Picked wrong option");
+        }
+    }
 
     this.isValid = function(option) {
         return contains(this.options,option);
@@ -51,17 +68,19 @@ function Choice (previous_choices) {
 
 function Sequence () {
     
-    
     this.pickColor = function(color) {
         clear_error();
         if (this.currChoice().isValid(color))
         {
+            this.currChoice().choose(color);
             log("Picked " + color);
 
             this.index++;
             if (this.index >= this.choices.length) {
                 this.index = 0;
-                this.newChoice();
+                if (randint(5)==0) {
+                    this.newChoice();
+                }
                 log("<br/>--new round--<br>");
             }
             this.currChoice().print();
@@ -81,13 +100,11 @@ function Sequence () {
     };
     
     this.type = "sequence";
-    this.options = ["red", "green"];
     this.choices = [];
     this.index = 0;
     
     this.newChoice();
     this.currChoice().print();
-
 }
 
 var sequence = new Sequence();
