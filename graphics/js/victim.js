dff7facedce30e2a5fc282d00011905708de05fa
renato-grottesi <renato.grottesi@gmail.com>
2016-01-30 17:43:22 +0100

    right callbacks

--- graphics/js/victim.js
+++ graphics/js/victim.js
@@ -7,23 +7,40 @@
 gameAssets.victimAssets = new Array();
 gameAssets.victimAssets[0] = { URL:'assets/drawings/sheep.png', name:'sheep', scale:{x : 0.07, y : 0.07} };
 gameAssets.victimAssets[1] = { URL:'assets/drawings/dead_sheep.png', name:'sheep_dead', scale:{x : 0.07, y : 0.07} };
+gameAssets.victimAssets[2] = { URL:'assets/drawings/bunny.png', name:'bunny', scale:{x : 0.07, y : 0.07} };
+gameAssets.victimAssets[3] = { URL:'assets/drawings/dead_bunny.tif', name:'bunny_dead', scale:{x : 0.07, y : 0.07} };
+gameAssets.victimAssets[4] = { URL:'assets/drawings/cat.tif', name:'cat', scale:{x : 0.07, y : 0.07} };
+gameAssets.victimAssets[5] = { URL:'assets/drawings/dead_cat.tif', name:'cat_dead', scale:{x : 0.07, y : 0.07} };
 
 function Victim(hit) {
   this.hit = hit;
+  this.sprite;
+  this.carContact = false;
+  this.hitVictim = null;
 
   this.init = function (blockCollisionGroup) {
-    this.sprite = game.add.sprite(300, 7000, gameAssets.victimAssets[0].name);
-    this.sprite.scale.x = gameAssets.victimAssets[0].scale.x;
-    this.sprite.scale.y = gameAssets.victimAssets[0].scale.y;
-    game.physics.p2.enable(this.sprite, false);
-    this.sprite.body.setCircle(16);
-    //this.sprite.body.debug = true;
-    this.sprite.body.mass = 10;
-    this.sprite.body.setCollisionGroup(blockCollisionGroup);
-    this.sprite.body.collides([blockCollisionGroup]);
-    this.sprite.sprite_collides = false;
-    this.sprite.body.onBeginContact.add(this.on_sprite_begin_contact, this, 0, 0, 0);
-    this.sprite.body.onEndContact.add(this.on_sprite_end_contact, this, 0, 0, 0);
+    this.sprite = new Array();
+    for(var i = 0; i<gameAssets.victimAssets.length; i++)
+    {
+      this.sprite[i] = game.add.sprite(300, 7000, gameAssets.victimAssets[i].name);
+      this.sprite[i].scale.x = gameAssets.victimAssets[i].scale.x;
+      this.sprite[i].scale.y = gameAssets.victimAssets[i].scale.y;
+      game.physics.p2.enable(this.sprite, false);
+      this.sprite[i].body.setCircle(16);
+      //this.sprite[i].body.debug = true;
+      this.sprite[i].body.mass = 10;
+      this.sprite[i].body.setCollisionGroup(blockCollisionGroup);
+      this.sprite[i].body.collides([blockCollisionGroup]);
+      this.sprite[i].sprite_collides = false;
+      this.sprite[i].father = this;
+      this.sprite[i].victim = gameAssets.victimAssets[i].name;
+      this.sprite[i].body.onBeginContact.add(this.on_sprite_begin_contact, this.sprite[i]);
+      this.sprite[i].body.onEndContact.add(this.on_sprite_end_contact, this.sprite[i]);
+      this.sprite[i].body.x = 250 + i * 60;
+    }
+
+    this.myText = game.add.text(20, 40, "hello!!", fontAssets.counterFontStyle);
+    this.myText.fixedToCamera = true;
   };
 
   this.neutral = function() {
@@ -36,24 +53,24 @@ function Victim(hit) {
   };
 
   this.update = function() {
-    this.sprite.body.angularVelocity = 1;
-    if(this.sprite.sprite_collides) {
-      this.sprite.body.velocity.y -= 50;
-    }
+    /*
+       this.sprite[i].body.angularVelocity = 1;
+       if(this.sprite[i].sprite_collides) {
+       this.sprite[i].body.velocity.y -= 50;
+       }
+     */
   };
 
-  this.on_sprite_begin_contact = function(a, b, c, d, e) {
-    this.sprite.sprite_collides = true;
+  this.on_sprite_begin_contact = function(body_a, body_b, c, d, e) {
+    this.father.myText.text += "\nkilled: " + this.victim;
 
-    this.sprite.body.y -= 500;
-    this.sprite.body.x = 200 + Math.random() * 400;
-
+    this.body.y -= 500;
+    this.body.x = 200 + Math.random() * 400;
     // Invoke the callback called hit() on the object called this.hit
-    this.hit.hit(this.hit);
+    this.father.hit.hit(this.father.hit);
   };
 
-  this.on_sprite_end_contact = function(a, b, c, d, e) {
-    this.sprite.sprite_collides = false;
+  this.on_sprite_end_contact = function(body_a, body_b, c, d, e) {
   };
 
 };
