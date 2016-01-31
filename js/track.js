f34bd2b47f0227a4be796447493b85456c57728c
renato-grottesi <renato.grottesi@gmail.com>
2016-01-31 13:43:20 +0100

    new finishing line

--- js/track.js
+++ js/track.js
@@ -6,7 +6,8 @@
 gameAssets.trackAssets = new Array();
 gameAssets.trackAssets[0] = { URL:'assets/drawings/road_1.png', name:'track' };
 gameAssets.trackAssets[1] = { URL:'assets/drawings/grass.png', name:'grass' };
-gameAssets.trackAssets[2] = { URL:'assets/drawings/finish_gate.png', name:'finish' };
+gameAssets.trackAssets[2] = { URL:'assets/drawings/finish_gate_top.png', name:'finish_top' };
+gameAssets.trackAssets[3] = { URL:'assets/drawings/finish_gate_bottom.png', name:'finish_bottom' };
 
 function Track(gameProperties) {
   this.gameProperties = gameProperties;
@@ -17,18 +18,23 @@ function Track(gameProperties) {
     this.grassSprite = game.add.tileSprite(0, 0, this.gameProperties.gameWidth, this.gameProperties.gameHeight, gameAssets.trackAssets[1].name);
     this.trackSprite = game.add.tileSprite(165, 0, 650, gameProperties.gameHeight, gameAssets.trackAssets[0].name);
     this.trackSprite.scale.x = 0.75;
-    this.finishSprite = game.add.sprite(165, 0, gameAssets.trackAssets[2].name);
-    this.finishSprite.scale.x = 0.75;
+    this.finishSpriteTop = game.add.sprite(165, 0, gameAssets.trackAssets[2].name);
+    this.finishSpriteTop.scale.x = 0.75;
+    this.finishSpriteBottom = game.add.sprite(165, 0, gameAssets.trackAssets[3].name);
+    this.finishSpriteBottom.scale.x = 0.75;
 
     this.reset();
   };
 
   this.showFinishingAt = function(y) {
-    this.finishSprite.y = y;
-    this.finishSprite.alpha = 1;
+    this.finishSpriteTop.y = y;
+    this.finishSpriteTop.alpha = 1;
+    this.finishSpriteBottom.y = y;
+    this.finishSpriteBottom.alpha = 1;
   }
 
   this.reset = function() {
-    this.finishSprite.alpha = 0;
+    this.finishSpriteTop.alpha = 0;
+    this.finishSpriteBottom.alpha = 0;
   }
 };
