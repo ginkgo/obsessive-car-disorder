4c1f9acc4a69bd284375f230f092384b8beed359
renato-grottesi <renato.grottesi@gmail.com>
2016-01-31 14:26:42 +0100

    explosion begin

--- js/game.js
+++ js/game.js
@@ -43,6 +43,8 @@ preload: function () {
                game.load.spritesheet(gameSheets[obj][ast].name, gameSheets[obj][ast].URL, 500, 500);
              }
            }
+
+           game.load.spritesheet("explosion", "assets/drawings/explosion_animation.png", 300, 300);
          },
 
 create: function () {
@@ -237,8 +239,6 @@ render: function() {
                     this.audioInterface.switchConfig([1,0,0,0], switchTime);
                 }
             }
-                
-                
         }
     }
 
